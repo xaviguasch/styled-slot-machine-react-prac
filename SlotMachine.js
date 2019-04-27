b085e4355d06e8c24c33f54c68923cd809154bb5
xaviguasch <xaviguasch@gmail.com>
2019-04-27 19:12:34 +0200

    toyed with different react styling options

--- SlotMachine.js
+++ SlotMachine.js
@@ -2,13 +2,14 @@ class SlotMachine extends React.Component {
   render() {
     const { s1, s2, s3 } = this.props
     const isWinner = s1 === s2 && s1 === s3
+    const colors = { fontSize: '50px', backgroundColor: 'purple' }
     return (
-      <div>
-        <p>
+      <div className='Machine'>
+        <p style={colors}>
           {s1} {s2} {s3}
         </p>
 
-        <p>{isWinner ? 'Winner!' : 'Sorry, dude... you lost!'}</p>
+        <p className={isWinner ? 'win' : 'lose'}>{isWinner ? 'Winner!' : 'Sorry, dude... you lost!'}</p>
       </div>
     )
   }
